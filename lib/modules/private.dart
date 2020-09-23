5a6a3b1a8deae027b76c8d06533f7630c16efe07
sigilai <briansigilai@gmail.com>
2020-09-23 12:13:43 +0300

    edited top nav for testing

--- lib/modules/private.dart
+++ lib/modules/private.dart
@@ -27,7 +27,7 @@ class _DashboardState extends State<Private> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                Text("Private",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,),),
+                Text("Privates",style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold,),),
                 VerticalDivider(),
                 GestureDetector(
                   onTap: (){
